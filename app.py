a8d909b7179003af11c71d97146ba349edf02612
puruarora12 <puruarora14@gmail.com>
2019-06-12 09:34:36 +0530

    minor changes

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def sms_reply():
         sender = request.form.get('From')
         nummedia = request.form.get('NumMedia')
         print(msg)
-        print(nummedia)
-        print("####################################")
+        #print(nummedia)
+        #print("####################################")
         s= ["I don't support this type of media yet" ,"Thanks for sending me Media but currently i don't support Media recognition will update you about this in future", "Media support isn't a feature yet" , "This Feature will be coming soon" , " Don't push so much pressure on me , I am just a newly born bot" ]
         if (int(nummedia) == 0):
 
@@ -32,16 +32,16 @@ def sms_reply():
             resp = MessagingResponse()
             # print(resp)
             # print(resp.message(fetch_reply(msg, sender )))
-            if msg ==' ':
+            if msg =='':
                 print('from empty body')
                 resp.message(s[randint(0 ,len(s)-1)])
                 return str(resp)
             elif str(msg).lower() == 'ok' or str(msg).lower() == 'okay':
 
-                print('in the emoji')
+                #print('in the emoji')
 
                 resp.message('😃')
-                print(resp)
+                #print(resp)
                 return str(resp)
             else:
                 pass
@@ -51,18 +51,18 @@ def sms_reply():
 
             if len(resp_str[1]) != 0:
                 resp.message(resp_str[0]).media(resp_str[1])
-                print(resp)
+                #print(resp)
             else:
                 resp.message(resp_str[0])
 
             return str(resp)
         else:
-            print('from else')
+            #print('from else')
             resp = MessagingResponse()
             resp.message(s[randint(0 , len(s)-1)])
             return str(resp)
 
-        print(type(msg))
+        #print(type(msg))
 
 
 
@@ -70,13 +70,13 @@ def sms_reply():
         resp = MessagingResponse()
             # print(resp)
             # print(resp.message(fetch_reply(msg, sender )))
-        print(type(resp))
+        #print(type(resp))
         resp_str=fetch_reply(msg,sender)
             #print('reply fetched')
 
         if len(resp_str[1])!=0:
             resp.message(resp_str[0]).media(resp_str[1])
-            print(resp)
+            #print(resp)
         else:
             resp.message(resp_str[0])
 
